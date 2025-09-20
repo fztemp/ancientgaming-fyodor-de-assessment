d5cd9c7951f96784a24b03e25f3428db876cbc35
fzabavin <fyodor@zabavin.tech>
2025-09-20 17:37:40 +0200

    Test commit to check whether Github action is properly configured

--- airflow/dags/pipeline.py
+++ airflow/dags/pipeline.py
@@ -7,5 +7,9 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 
 
-with DAG('pipeline', start_date=datetime(2025, 9, 1), catchup=False) as dag:
+with DAG(
+    'pipeline',
+    start_date=datetime(2025, 9, 1),
+    catchup=False,
+) as dag:
     t1 = EmptyOperator(task_id='placeholder_task')
